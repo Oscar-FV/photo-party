1afe992371bbfac382cfc3d1d475413cdf8aeb3d
Oscar-FV <oscar.flores.vqz@gmail.com>
2024-10-26 13:53:36 -0600

    added ngrok paath for prod

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['images.unsplash.com', 'source.unsplash.com', 'picsum.photos','127.0.0.1', 'localhost'],
+        domains: ['images.unsplash.com', 'source.unsplash.com', 'picsum.photos','127.0.0.1', 'localhost', '2805-187-245-73-217.ngrok-free.app'],
       },
 };
 
