a53d2ac40042f4478ef92781bf164960cff54bff
Oscar-FV <oscar.flores.vqz@gmail.com>
2024-10-26 16:26:11 -0600

    fixed erro for images

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['images.unsplash.com', 'source.unsplash.com', 'picsum.photos','127.0.0.1', 'localhost', 'https://2724-187-245-73-217.ngrok-free.app'],
+        domains: ['images.unsplash.com', 'source.unsplash.com', 'picsum.photos','127.0.0.1', 'localhost', '2724-187-245-73-217.ngrok-free.app'],
       },
 };
 
